bfe3b6a5b3dc7c399247c676c32864293926ce75
anujvishwakarma07 <anujvishwakarma7077@gmail.com>
2026-07-15 19:11:36 +0530

    feat: Implemented local push command & aws s3 integration, integrated AWS S3 configuration in aws-conf.js pointing to s3 bucket,

--- backend/controllers/push.js
+++ backend/controllers/push.js
@@ -1,5 +1,39 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { S3_BUCKET, s3 } = require('../config/aws-config');
+
 async function pushCode(params) {
-    console.log("Code pushed successfully");
+    const repoPath = path.resolve(process.cwd(), ".mygit");
+    const commitsPath = path.join(repoPath, "commits");
+    
+
+
+    try {
+        const commitDirs = await fs.readdir(commitsPath);
+        
+        for(const commitDir of commitDirs) {
+            const commitPath = path.join(commitsPath, commitDir);
+            
+            const files = await fs.readdir(commitPath);
+            for(const file of files) {
+                const filePath = path.join(commitPath, file);
+                const fileContent = await fs.readFile(filePath);
+                const params = {
+                    Bucket: S3_BUCKET,
+                    Key: `commits/${commitDir}/${file}`,
+                    Body: fileContent,
+                }
+
+                await s3.upload(params).promise();
+            }
+        }
+
+        console.log("All commits pushed to S3");
+        
+
+    } catch (error) {
+        console.error("Error on pushing to s3", error);
+    }
 }
 
 module.exports = {pushCode};
